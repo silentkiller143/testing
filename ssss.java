ba3ffd61dae282582e69ec20bd8f331aa09a1f0d
codetech-shivam <ss2205788@gmail.com>
2023-11-01 22:57:03 +0530

    fhsh

---
+++ ssss.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ssss {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows for the triangle: ");
+        int numRows = scanner.nextInt();
+        scanner.close();
+
+        for (int i = 1; i <= numRows; i++) {
+            for (int j = 1; j <= numRows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1 || i == numRows) {
+                    System.out.print(i);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
